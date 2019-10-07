810cd5aa5d5e9b13bbfa79079b2dfd9869e10d64
Sherius <Sheriusek@wp.pl>
2019-10-07 22:17:27 +0200

    Second Chalenge

--- kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -1,6 +1,17 @@
 package com.kodilla.exception.test;
 
 public class ExceptionHandling  {
+    public static void main(String[] args){
+        SecondChallenge secondChallenge = new SecondChallenge();
+
+        try {
+            System.out.println(secondChallenge.probablyIWillThrowException(0.5, 1.5));
+        } catch (Exception e){
+            System.out.println("Exception:" + e);
+        } finally {
+            System.out.println("Progra ended");
+        }
+    }
 
 
 
